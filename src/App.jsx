4b3827a122f57d116e0a3c693c124ee830c198bd
sajna <sajnamolsanj346@gmail.com>
2025-09-03 16:06:37 +0530

    counter button updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const onIncrement = () => {
-    count < 10 ? setCount((prev) => prev + 1) : 0;
+    count < 10 ? setCount((prev) => prev + 1) : alert("Maximum count is 10");
   };
   const onDecrement = () => {
-    count > 0 ? setCount((prev) => prev - 1) : 0;
+    count > 0 ? setCount((prev) => prev - 1) : alert("Minimum count is 0");
   };
 
   return (
     <>
-      <h3> Counter App {count} </h3>
+      <h3> Counter App</h3>
+      <h1>{count}</h1>
       <button onClick={onIncrement}>Up </button>
       &nbsp;&nbsp;&nbsp;
       <button onClick={onDecrement}>Down</button>
